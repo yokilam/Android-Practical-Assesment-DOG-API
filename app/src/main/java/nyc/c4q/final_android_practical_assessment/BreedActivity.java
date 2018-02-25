81ab322e819f64ce0eb31d15805f5e3e9de4d81b
Yoki Lam <yokeyenlam@ac.c4q.nyc>
2018-02-25 12:30:16 -0500

    finished login

--- app/src/main/java/nyc/c4q/final_android_practical_assessment/BreedActivity.java
+++ app/src/main/java/nyc/c4q/final_android_practical_assessment/BreedActivity.java
@@ -1,13 +1,36 @@
 package nyc.c4q.final_android_practical_assessment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BreedActivity extends AppCompatActivity {
+
+    private TextView welcomeUserMessage;
+    private SharedPreferences loginSharePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breed);
+
+        welcomeUserMessage= findViewById(R.id.welcome_user_textView);
+
+        Intent intent= getIntent();
+        loginSharePref=getApplicationContext().getSharedPreferences(intent.getStringExtra("loginPrefKey"), MODE_PRIVATE);
+
+        String checkUser= loginSharePref.getString("user", null);
+
+        StringBuilder welcomeMessage= new StringBuilder();
+        welcomeMessage.append("What kind of dog would you like to see, ").append(checkUser).append("?");
+
+        if(checkUser!=null) {
+            welcomeUserMessage.setText(welcomeMessage.toString());
+        } else {
+            Intent backToLoginIntent= new Intent(BreedActivity.this, LoginActivity.class);
+            startActivity(backToLoginIntent);
+        }
     }
 }
--- app/src/main/java/nyc/c4q/final_android_practical_assessment/LoginActivity.java
+++ app/src/main/java/nyc/c4q/final_android_practical_assessment/LoginActivity.java
@@ -8,43 +8,53 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private EditText username, password;
     private Button submitButton;
     private SharedPreferences loginSharePreference;
-    private static final String SHARE_PREF_KEY="loginSharedPrefenceres";
+    private static final String SHARED_PREF_KEY="loginSharedPrefenceres";
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         setUpViews();
+        String checkUser= loginSharePreference.getString("user", null);
+
+        if(checkUser!= null){
+            intentToBreedActivity();
+        }
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = loginSharePreference.edit();
                 if (username.getText().toString() == null) {
                     username.setError("Please enter a username!");
                 } else if (password.getText().toString() == null) {
                     password.setError("Please enter a password");
-                } else if (password.getText().toString().contains(username.getText().toString())) {
+                } else if (password.getText().toString().contains(username.getText().toString()) ||
+                        username.getText().toString().contains(password.getText().toString())) {
                     password.setError("The password cannot contain username");
                 } else {
                     if (!password.getText().toString().contains(username.getText().toString())) {
-                        editor.putString("user" + username.getText().toString(), username.getText().toString());
-                        editor.putString("password" + username.getText().toString(), password.getText().toString());
-                        editor.putBoolean("isLoggedIn" + username.getText().toString(), true);
+                        editor.putString("user", username.getText().toString());
+                        editor.putString("password", password.getText().toString());
+                        editor.putBoolean("isLoggedIn", true);
                         editor.commit();
-                        Intent intent= new Intent(MainActivity.this, BreedActivity.class);
-                        intent.putExtra("username", username.getText().toString());
-                        startActivity(intent);
+                        intentToBreedActivity();
                     }
                 }
             }
         });
+    }
+
+    private void intentToBreedActivity() {
+        Intent intent= new Intent(LoginActivity.this, BreedActivity.class);
+        intent.putExtra("loginPrefKey", SHARED_PREF_KEY);
+        startActivity(intent);
     }
 
     private void setUpViews() {
@@ -52,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         password= findViewById(R.id.password_edittext);
         submitButton= findViewById(R.id.submit_button);
 
-        loginSharePreference= getApplicationContext().getSharedPreferences(SHARE_PREF_KEY, MODE_PRIVATE);
+        loginSharePreference= getApplicationContext().getSharedPreferences(SHARED_PREF_KEY, MODE_PRIVATE);
+        editor = loginSharePreference.edit();
     }
 }
